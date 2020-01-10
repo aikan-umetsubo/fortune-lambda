00e7cad3a413f2cb633dd37ffa3a16ce918c3d25
aikan-umetsubo <yakinori21@gmail.com>
2020-01-10 12:38:36 +0900

    Add: added selector.js and fixed index.js to use it. And removed const.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const fs = require('fs');
-const messages = require('./messages');
+const selector = require('./src/selector');
 
 export function handler(event, context, callback) {
-  fs.readFile('./messages/art/00000', (err, data) => {
+  // get randomly selected category and index
+  const selector = selector.setOption("");
+  const filePath = selector.select();
+
+  fs.readFile(filePath, (err, data) => {
     response(data, callback);
   });
 }
---
+++ src/selector.js
@@ -0,0 +1,12 @@
+export const selector = {
+  set: (option) => {
+
+  },
+  select: () => {
+    return './messages/art/00000';
+  },
+  option: {
+    offensive: false,
+    all: false
+  }
+};
